6e0b7eaaeb6c38af1daac6895487881a51454436
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2026-04-26 21:35:48 +0300

    preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,2 +1,12 @@
 # this where the dataset will be cleaned and classes 3-8 will be binned into
 # three classes (low, medium, high)
+# the purpos of preprocess is to get thedataset ready for training / 
+
+from pathlib import Path
+import pandas as pd
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_PATH = BASE_DIR / "data" / "winequalityred.csv"
+
+
+
