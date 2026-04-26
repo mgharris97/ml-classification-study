8d25e59c6a719fbea61f1fcdc11f802fe175b6f5
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2026-04-26 22:51:07 +0300

    preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,12 +1,20 @@
-# this where the dataset will be cleaned and classes 3-8 will be binned into
-# three classes (low, medium, high)
-# the purpos of preprocess is to get thedataset ready for training / 
+# Purpose: Clean the dataset and fix classification using Pandas
+# Pathlib: Used to create a relative filepath regardless of OS
+# Note: The dataset uses semicolons (;) as a seperator not commas (,)
 
 from pathlib import Path
 import pandas as pd
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_PATH = BASE_DIR / "data" / "winequalityred.csv"
+DATA_PATH = BASE_DIR / "data" / "winequality-red.csv"
+
+df = pd.read_csv(DATA_PATH, sep=";")
+
+# Explore the dataset to see if Pandas parses it correctly
+print(df.shape) #print number of columns and rows
+print(df.dtypes) #return each column and its datatype
+print(df.head()) # returns the first five rows
+
 
 
 
