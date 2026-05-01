99a97ca6f50e119b2ed1999a4229e1966d8587b6
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2026-05-01 12:44:55 +0300

    added aNN

--- src/ann.py
+++ src/ann.py
@@ -1,11 +1,10 @@
 from pathlib import Path
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.cluster import AgglomerativeClustering
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_PATH_NORMALIZED = BASE_DIR / "data" / "winequality-red-normalized.csv"
 
 df = pd.read_csv(DATA_PATH_NORMALIZED, sep=";")
-
-#code here
