35613e3169fc7ff215955d894dd368c6a2f39340
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2026-05-05 22:56:40 +0300

    fixed convergance  warnings in aNN

--- src/ann.py
+++ src/ann.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import warnings
+from sklearn.exceptions import ConvergenceWarning
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_PATH_NORMALIZED = BASE_DIR / "data" / "winequality-red-normalized.csv"
